72fee25f2cda529e9cac4665aa9e9aad3bfb66df
p.cabezuela <p.cabezuela@1DAW-01.BUINF.local>
2026-04-08 10:16:03 +0200

    Ejercicio libreria

--- Enum-Ej2-Biblioteca-pcabezuela2/src/main/java/com/gsd/programacion/App.java
+++ Enum-Ej2-Biblioteca-pcabezuela2/src/main/java/com/gsd/programacion/App.java
@@ -1,12 +1,60 @@
 package com.gsd.programacion;
-
+import java.util.*;
 public class App {
     public static void main(String[] args) {
         Libreria lib1 = new Libreria();
-        Libro l1 = new Libro ("Cenicienta", "manolo", 2002, Categoria.POESIA);
-        
-        lib1.aniadirLibro(l1);
-        lib1.aniadirLibro(l1);
-        lib1.mostrarLibrosCategoria(Categoria.POESIA);
+        Scanner teclado = new Scanner (System.in);
+        int num;
+        do {
+        	System.out.println("----  MENU BIBLIOTECA ----");
+        	System.out.println("1- Añadir Nuevo Libro");
+        	System.out.println("2- Mostrar Libros de una categoría determinada");
+        	System.out.println("3- Mostrar todos los libros");
+        	System.out.println("4- Salir del Programa");
+        	num = teclado.nextInt();
+        	switch(num) {
+        		case 1:
+        			 teclado.nextLine(); 
+        			    
+        			    System.out.println("Dime el titulo del libro: ");
+        			    String titulo = teclado.nextLine(); 
+        			    
+        			    System.out.println("Dime el autor del Libro: ");
+        			    String autor = teclado.nextLine();
+        			    
+        			    System.out.println("Año de publicación del libro: ");
+        			    int anio_publicacion = teclado.nextInt();
+        			    teclado.nextLine();
+        			    
+        			    System.out.println("Dime la categoría (POESIA/CIENCIA_FICCION/NOVELA/HISTORIA): ");
+        			    String categoriaInput = teclado.next().toUpperCase().trim();
+        			       			 
+        			    try {
+        			    	Categoria categoriaIntorducida = Categoria.valueOf(categoriaInput);
+        			    	Libro nuevoLibro = new Libro(titulo, autor, anio_publicacion, categoriaIntorducida );
+            			    lib1.aniadirLibro(nuevoLibro);
+        			    }catch (IllegalArgumentException e) {
+            		        System.err.println("Error: La categoría '" + categoriaInput + "' no es válida.");
+            		    }		   
+        			    break;
+        		case 2:
+        		    System.out.println("¿Qué categoría quieres ver? (POESIA, CIENCIA_FICCION, NOVELA, HISTORIA):");
+        		    String categoriaVer = teclado.next().toUpperCase().trim();
+
+        		    try {
+        		        Categoria catEncontrada = Categoria.valueOf(categoriaVer);
+        		        lib1.mostrarLibrosCategoria(catEncontrada);
+        		    } catch (IllegalArgumentException e) {
+        		        System.err.println("Error: La categoría '" + categoriaVer + "' no es válida.");
+        		    }
+        		    break;
+        		case 3:
+        			lib1.mostrarLibros();
+        			break;
+        		case 4: 
+        			System.out.println("Chao pescao");
+        			return;
+        	}
+        }while(num != 4);
     }
 }
--- Enum-Ej2-Biblioteca-pcabezuela2/src/main/java/com/gsd/programacion/Libreria.java
+++ Enum-Ej2-Biblioteca-pcabezuela2/src/main/java/com/gsd/programacion/Libreria.java
@@ -10,21 +10,30 @@ public class Libreria {
 	}
 	
 	public boolean aniadirLibro(Libro libro) {
-		if(!libreria.contains(libro.getTitulo())) {
-			libreria.add(libro);
-			System.out.println("Libro " + libro.getTitulo() + " añadido a la libreria.");
-			return true;
+		for(Libro l1 : libreria) {
+			if(l1.getTitulo().equalsIgnoreCase(libro.getTitulo())) {
+				System.err.println("El libro " + libro.getTitulo() + " ya exite en la librería");
+				return false;
+			}
 		}
-		System.err.println("El libro ya exite en la librería");
-		return false;
+		libreria.add(libro);
+		System.out.println("Libro " + libro.getTitulo() + " añadido a la libreria.");
+		return true;		
 	}
 	
 	public void mostrarLibrosCategoria (Categoria cat) {
 		System.out.println("---- LIBROS PARA lA CATEGORÍA " + cat.toString());
 		for(Libro l1 : libreria) {
 			if(l1.getCategoria().equals(cat)) {
-				System.out.println("-" + l1.getTitulo() + "\n");
+				System.out.println("-" + l1.getTitulo());
 			}
+		}
+	}
+	
+	public void mostrarLibros() {
+		System.out.println("---- LIBROS DE TODA LA BIBLIOTECA ---");
+		for(Libro l1 : libreria) {
+			System.out.println(" - " + l1.getTitulo() );
 		}
 	}
 }
