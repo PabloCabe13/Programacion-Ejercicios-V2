6ac168dfab223738dd15a343fe9a3e1f4e792367
p.cabezuela <p.cabezuela@1DAW-01.BUINF.local>
2026-04-14 11:46:23 +0200

    Ej Poblacion Enum

---
+++ Enum-Ej4-Poblacion-pcabezuela2/src/main/java/com/gsd/programacion/Continentes.java
@@ -0,0 +1,18 @@
+package com.gsd.programacion;
+
+public enum Continentes {
+	AFRICA (1578),
+	AMERICA (1057),
+	ASIA (4848),
+	EUROPA (745),
+	OCEANIA (46),
+	ANTARIDA (100);
+	
+	private int poblacionContinente;
+
+	private Continentes(int poblacionContinente) {
+		this.poblacionContinente = poblacionContinente;
+	}
+	
+	ñ
+}
