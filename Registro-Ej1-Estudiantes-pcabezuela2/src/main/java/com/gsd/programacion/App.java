afd8401eb0aa04feaf5be4981be687b2b1510739
p.cabezuela <p.cabezuela@1DAW-01.BUINF.local>
2026-04-17 11:01:36 +0200

    Ejercicio registro estudiantes hecho (faltaria eliminar pero da igual)

--- Registro-Ej1-Estudiantes-pcabezuela2/src/main/java/com/gsd/programacion/App.java
+++ Registro-Ej1-Estudiantes-pcabezuela2/src/main/java/com/gsd/programacion/App.java
@@ -7,6 +7,26 @@ public class App {
     	Persona p1 = new Persona("Pepe", 1234, 15, "1DAW", new ArrayList<Double>());
     	Persona p2 = new Persona("Ana", 2547, 10, "1DAW", new ArrayList<Double>());
     	
+    	Escuela es1 = new Escuela();
     	
+    	es1.aniadirEstudiante(p1);
+    	es1.aniadirEstudiante(p2);
+    	
+    	es1.mostrarEstudiantes();
+    	
+    	System.out.println();
+    	es1.aniadirNota(1234, 10);
+    	es1.aniadirNota(1234, 7);
+    	es1.aniadirNota(2547, 8);
+    	es1.aniadirNota(234, 5.5);
+    	
+    	es1.mostrarEstudiantes();
+    	
+    	System.out.println();
+    	es1.actualizarInfoEstu(1234, 18, "2DAW");
+    	es1.buscarEstudiante("pepe");
+    	
+    	System.out.println();
+    	es1.calcularMediaNotas();
     }
 }
--- Registro-Ej1-Estudiantes-pcabezuela2/src/main/java/com/gsd/programacion/Escuela.java
+++ Registro-Ej1-Estudiantes-pcabezuela2/src/main/java/com/gsd/programacion/Escuela.java
@@ -31,10 +31,57 @@ public class Escuela {
 	}
 	
 	public void mostrarEstudiantes() {
-		System.out.println("------- ALUMNOS DEL COLEGIO -------");
+		System.out.println("\n------- ALUMNOS DEL COLEGIO -------");
 		for(int matricula : estudiantes.keySet()) {
 			Persona estudiante = estudiantes.get(matricula);
-			System.out.println(estudiante.toString());
+			System.out.println("- " + estudiante.toString());
 		}
+	}
+	
+	public void buscarEstudiante(String nombre){
+		for(int matricula : estudiantes.keySet()) {
+			Persona estudiante = estudiantes.get(matricula);
+			if(estudiante.nombre().equalsIgnoreCase(nombre)) {
+				System.out.println("\n---- ESTUDIANTE " + estudiante.nombre() + " ----");
+				System.out.println("- " + estudiante.toString());
+				return;
+			}
+		}
+		System.err.println("No existe ese alumno");
+		return;
+	}
+	
+	public boolean actualizarInfoEstu(int NumMatricula, int NuevaEdad, String grado) {
+		for(int matricula : estudiantes.keySet()) {
+			if(matricula == NumMatricula) {
+				Persona estudiante = estudiantes.get(matricula);
+				Persona estuActualizado = new Persona (estudiante.nombre(), estudiante.numMatricula(), NuevaEdad, grado, estudiante.notas());
+				estudiantes.put(matricula, estuActualizado);
+				System.out.println("Estudiante actualizado");
+				return true;
+			}
+		}
+		System.err.println("Error");
+		return false;
+	}
+	
+	public void calcularMediaNotas () {
+		int estus = 0;
+		double MediaFinal = 0;
+		for(int m : estudiantes.keySet()) {
+			Persona estudiante = estudiantes.get(m);
+			int NumNotas = 0;
+			double notas = 0;
+			for(Double nota : estudiante.notas()) {
+				notas += nota;
+				NumNotas++;
+			}
+			double MediaEstudiante = (double) notas / NumNotas;
+			MediaFinal += MediaEstudiante;
+			estus++;
+		}
+		
+		MediaFinal /= estus;
+		System.out.println("La nota media de todos los alumnos es " + MediaFinal);
 	}
 }
