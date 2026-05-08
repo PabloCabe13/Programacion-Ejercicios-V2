a5667f44273f6677617f49801e0b8efed9e05afe
p.cabezuela <p.cabezuela@1DAW-01.BUINF.local>
2026-05-08 11:14:44 +0200

    Mision Espacial 4

--- PC-MisionEspacial-pcabezuela2/src/main/java/com/gsd/programacion/App.java
+++ PC-MisionEspacial-pcabezuela2/src/main/java/com/gsd/programacion/App.java
@@ -1,15 +1,37 @@
 package com.gsd.programacion;
 
+import java.util.*;
+
 public class App {
     public static void main(String[] args) {
-        try {
-			Destino d1 = new Destino ("Messi", 10);
-			Exploradora a2 = new Exploradora("Manolo", 50, 5, d1);
-			Militar m1 = new Militar("Militar", 80, 5, null, 50);
-			m1.viajar(d1);
-			m1.mostrarReporte();
-		} catch (EstadisticaInvalidaException | CombustibleInsuficienteException e) {
+    	if(args.length != 1) {
+    		System.err.println("Argumentos Vacíos");
+    	}
+        //try {
+        	
+			for(int i = 0 ; i < args.length; i++) {
+				 String datosLimpios = args[i].replace("naves:", "").replace("destinos:", "");
+				 String [] partes = datosLimpios.split(";");	
+				 String [] naves = partes[0].split(",");
+				 String [] destinos = partes[1].split(",");
+				 
+				 System.out.println(Arrays.toString(naves));
+				 System.out.println(Arrays.toString(destinos));
+				 
+				 for (int j = 0; j < naves.length; j++) {
+					String [] nave = naves[i].split("-");
+					switch(nave[0]) {
+						case "exploradora":
+							Exploradora exploradora = new Exploradora();
+					}
+				}
+			}
+			
+			
+			
+			
+		/*} catch (EstadisticaInvalidaException | CombustibleInsuficienteException e) {
 			System.err.println("Error: "+ e.getMessage()); 
-		} 
+		} */
     }
 }
